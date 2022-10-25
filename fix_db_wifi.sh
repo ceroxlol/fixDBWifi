f52e95db4bb6cb4630fcf5c8070e64511c9a1ac7
Christopher Werner <christopher.werner@netlight.com>
2022-10-25 14:05:48 +0200

    fix error where I missed the case of having 2 interfaces when connected to the wifi@db. Take only the bridge and work with the bridge iface.

--- fix_db_wifi.sh
+++ fix_db_wifi.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 ip_gateway=$(ip route show default | awk '/default/ {print $3}')
+echo "ip gateway: $ip_gateway"
 ip_gateway=${ip_gateway::-1}0
-bridge=$(route -n | grep "$ip_gateway" | awk '{print $8}')
-echo "$bridge"
+echo "ip gateway last digit cut: $ip_gateway"
+bridge=$(route -n | grep "$ip_gateway" | grep "br-" | awk '{print $8}')
+echo "bridge: $bridge"
 if [[ $bridge != br-* ]]
 then
-	echo "No bridge found. Something went wrong. Aborting..."
+	echo "No bridge found. Something went wrong. Abort..."
+	exit;
+elif [[ $bridge = wl* ]]
+then
+	echo "Bridge most likely down already. Abort..."
 	exit;
 fi
-#These need sudo rights
-ifconfig $bridge down  
+echo "ifconfig $bridge down..."
+ifconfig $bridge down
+echo "brctl delbr $bridge..."
 brctl delbr $bridge
